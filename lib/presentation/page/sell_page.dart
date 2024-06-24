447633386965d077d7f2d76c5fee578989805419
bogibekdev <bogibekdev@gmail.com>
2024-06-24 21:10:35 +0500

    Commit information: 1. sell page not finished

--- lib/presentation/page/sell_page.dart
+++ lib/presentation/page/sell_page.dart
@@ -37,8 +37,17 @@ class _SellPageState extends State<SellPage> {
   ];
   Buyer? selectedBuyer;
   final TextEditingController buyerController = TextEditingController();
+  final TextEditingController compensationC = TextEditingController();
+  final TextEditingController prePriceC = TextEditingController();
+  final TextEditingController periodC = TextEditingController();
+  final TextEditingController remainingPriceC = TextEditingController();
+  final TextEditingController priceC = TextEditingController();
+
   FocusNode focusNode = FocusNode();
   bool isFocused = false;
+  bool isNoBuyer = false;
+  List<String> paymentTypes = ["cash", "credit"];
+  String paymentType = "";
 
   @override
   void initState() {
@@ -61,82 +70,289 @@ class _SellPageState extends State<SellPage> {
           style: TextStyle(color: ResColors.black),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: buyerController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: ResColors.textFieldBg,
-                    border: const OutlineInputBorder(),
-                    hintText: "Haridorni tanlang",
-                    label: const Text("Haridor"),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        buyerController.text = "";
-                      },
-                      icon: const Icon(Icons.close),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: buyerController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: ResColors.textFieldBg,
+                      border: const OutlineInputBorder(),
+                      hintText: "Xaridorni tanlang",
+                      label: const Text("Xaridor"),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          buyerController.text = "";
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
                     ),
+                    focusNode: focusNode,
+                    onChanged: (value) {},
                   ),
-                  focusNode: focusNode,
-                  onChanged: (value) {},
-                ),
-
-                Stack(
-                  children: [
-                    Column(
-                      children: [
-                        TextFormField(),
-                        TextFormField(),
-                        TextFormField(),
-                        TextFormField(),
-                        TextFormField(),
-                        TextFormField(),
-                      ],
-                    ),
-                    isFocused
-                        ? Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      decoration: const BoxDecoration(
-                          color: ResColors.textFieldBg,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10.0),
-                            bottomRight: Radius.circular(10.0),
-                          )),
-                      height: 200,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListView.builder(
-                          itemCount: buyers.length,
-                          itemBuilder: (context, position) => GestureDetector(
-                            onTap: () {
-                              focusNode.unfocus();
-                              buyerController.text =
-                              "${buyers[position].firstName} ${buyers[position].lastName}";
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
+                  Stack(
+                    children: [
+                      Column(
+                        children: [
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: priceC,
+                            maxLength: 15,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              counterText: "",
+                              filled: true,
+                              fillColor: ResColors.textFieldBg,
+                              border: const OutlineInputBorder(),
+                              hintText: "Umumiy summa : ",
+                              label: const Text("Umumiy summa"),
+                            ),
+                            style: const TextStyle(
+                              color: ResColors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: compensationC,
+                            maxLength: 15,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              counterText: "",
+                              filled: true,
+                              fillColor: ResColors.textFieldBg,
+                              border: const OutlineInputBorder(),
+                              hintText: "Kompensatsiya puli : ",
+                              label: const Text("Kompensatsiya puli"),
+                            ),
+                            style: const TextStyle(
+                              color: ResColors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          paymentTypes.isNotEmpty
+                              ? DropdownButtonFormField(
+                                  dropdownColor: ResColors.textFieldBg,
+                                  decoration: const InputDecoration(
+                                    filled: true,
+                                    fillColor: ResColors.textFieldBg,
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  hint: Text(
+                                    "To'lov turini tanlang",
+                                  ),
+                                  items: paymentTypes
+                                      .map((e) => DropdownMenuItem(
+                                          value: e,
+                                          child: Text(
+                                            e,
+                                            style: TextStyle(fontSize: 20),
+                                          )))
+                                      .toList(),
+                                  onChanged: (value) {
+                                    paymentType = value ?? "";
+                                    setState(() {});
+                                  },
+                                )
+                              : Container(),
+                          paymentType == paymentTypes[1]
+                              ? Column(
+                                  children: [
+                                    const SizedBox(height: 16),
+                                    Text(
+                                      "Kredit ma'lumotlari",
+                                      style: const TextStyle(
+                                        color: ResColors.black,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 16),
+                                    TextFormField(
+                                      controller: prePriceC,
+                                      maxLength: 15,
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        counterText: "",
+                                        filled: true,
+                                        fillColor: ResColors.textFieldBg,
+                                        border: const OutlineInputBorder(),
+                                        hintText: "100 000 000",
+                                        label: Text("Oldindan to'lov"),
+                                      ),
+                                      style: const TextStyle(
+                                        color: ResColors.black,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 16),
+                                    TextFormField(
+                                      controller: remainingPriceC,
+                                      maxLength: 15,
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        counterText: "",
+                                        filled: true,
+                                        fillColor: ResColors.textFieldBg,
+                                        border: const OutlineInputBorder(),
+                                        hintText: "100 000 000",
+                                        label: Text("Qoldiq summa"),
+                                      ),
+                                      style: const TextStyle(
+                                        color: ResColors.black,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 16),
+                                    TextFormField(
+                                      controller: periodC,
+                                      maxLength: 4,
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        counterText: "",
+                                        filled: true,
+                                        fillColor: ResColors.textFieldBg,
+                                        border: const OutlineInputBorder(),
+                                        hintText: "48",
+                                        label: Text("Kredit muddati"),
+                                      ),
+                                      style: const TextStyle(
+                                        color: ResColors.black,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Container(),
+                          SizedBox(height: 16),
+                          SizedBox(
+                            width: MediaQuery.sizeOf(context).width,
+                            height: 60,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: ResColors.mainColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              onPressed: () {},
                               child: Text(
-                                "${buyers[position].firstName} ${buyers[position].lastName}",
-                                style:
-                                const TextStyle(color: ResColors.black),
+                                "Sotish",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: ResColors.white,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    )
-                        : Container(),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                      isFocused
+                          ? Container(
+                              width: MediaQuery.sizeOf(context).width,
+                              margin: const EdgeInsets.only(top: 10),
+                              decoration: const BoxDecoration(
+                                color: ResColors.textFieldBg,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              height: 200,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListView.builder(
+                                  itemCount: buyers.length,
+                                  itemBuilder: (context, position) =>
+                                      GestureDetector(
+                                    onTap: () {
+                                      selectedBuyer = buyers[position];
+                                      buyerController.text =
+                                          "${buyers[position].firstName} ${buyers[position].lastName}";
+                                      focusNode.unfocus();
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "${buyers[position].firstName} ${buyers[position].lastName}",
+                                        style: const TextStyle(
+                                            color: ResColors.black),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : isNoBuyer
+                              ? Container(
+                                  width: MediaQuery.sizeOf(context).width,
+                                  margin: EdgeInsets.only(top: 10),
+                                  decoration: const BoxDecoration(
+                                      color: ResColors.textFieldBg,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  height: 200,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text(
+                                        "Xaridor topilmadi!",
+                                        style: TextStyle(
+                                            color: ResColors.black,
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        "Iltimos, xaridorni qo'shish uchun tugmani bosing",
+                                        style: TextStyle(
+                                            color: ResColors.black,
+                                            fontSize: 14),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.sizeOf(context).width -
+                                                100,
+                                        height: 50,
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: ResColors.mainBg,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                            ),
+                                          ),
+                                          onPressed: () {},
+                                          child: Text(
+                                            "Xaridor qo'shish",
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              color: ResColors.black,
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : Container(),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
